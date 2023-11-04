b369d33120020a0c0ce6a4e36abc8c91ef1c99fb
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-11-04 13:17:50 -0300

    funcao de falar pronta

--- public/chat.js
+++ public/chat.js
@@ -12,10 +12,10 @@ socket.emit('novoUsuario', {
 const nomeDeUsuario = document.getElementById('nomeUsuario');
 nomeDeUsuario.innerHTML = `Bem vindo ${nomeUsuario}`
 
-let inputMsg = document.getElementById('inputMsg');
+const inputMsg = document.getElementById('inputMsg');
 
 inputMsg.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === 'Enter') {
         const mensagem = event.target.value;
         console.log(mensagem);
         data = {
@@ -34,8 +34,57 @@ socket.on('mensagem', data => {
 
     msgDiv.innerHTML += `
     <div class="novaMensagem">
-    <label class="form-label">
-        <strong> ${data.nome} </strong> <span> ${data.msg}</span>
-    </label>
+        <label class="form-label">
+            <strong> ${data.nome} </strong> <output id="novaMensagem">${data.msg}</output>
+        </label>
     </div>`
 });
+
+//////////////////////////////////////////////////////////////////////////
+
+
+const sintetizador = window.speechSynthesis;
+
+function ouvir() {
+    let texto = document.getElementById('novaMensagem').value;
+    let voz = sintetizador.getVoices();
+
+    if (voz.length !== 0) {
+        console.log('falando');
+        let msg = new SpeechSynthesisUtterance();
+        msg.voz = voz[0]; //primeira voz
+        msg.rate = 1; //velocidade
+        msg.pitch = 1; //tom
+        msg.text = texto;
+        msg.lang = 'pt-BR'
+        sintetizador.speak(msg)
+    }
+}
+
+const botaoOuvir = document.getElementById('ouvirMsg');
+
+botaoOuvir.onclick = ouvir;
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+function falar() {
+    if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+        let minhaFala = new SpeechRecognition();
+        minhaFala.lang = 'pt-BR';
+
+        minhaFala.start();
+
+        minhaFala.addEventListener('result', (event) => {
+            var result = event.results[0][0].transcript;
+            console.log(result);
+            inputMsg.value = result;
+        });
+    }
+}
+
+const botaoFalar = document.getElementById('falarMsg');
+
+botaoFalar.onclick = falar;
