f9ca15cbd36180d699b94df2910332df401b097e
Jônatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2025-08-02 20:43:11 -0300

    refs(main): redefinindo caminho dirname

--- src/http.js
+++ src/http.js
@@ -2,6 +2,11 @@ import express from 'express';
 import path from 'path';
 import http from 'http';
 import { Server } from "socket.io";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const porta = 8000;
 const app = express();
