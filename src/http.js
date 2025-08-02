b2261e1af66865be880712f7b4c8ac851703fc88
Jônatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2025-08-02 20:39:37 -0300

    refs(main): alterando imports e variaveis http para deploy no render

--- src/http.js
+++ src/http.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
+import http from 'http';
+import { Server } from "socket.io";
 
 const porta = 8000;
 const app = express();
-const serverHttp = require('http').createServer(app);
-const io = require('socket.io')(serverHttp);
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp);
 
 app.use(express.static(path.join(__dirname, '..', '/')));
 
