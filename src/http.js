bab5ee4574ca63e4f4108b633266ff791eb7ac3d
Jônatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2025-08-02 21:21:43 -0300

    feat(main): cors para comunicação client-server

--- src/http.js
+++ src/http.js
@@ -10,7 +10,17 @@ const __dirname = path.dirname(__filename);
 const porta = 8000;
 const app = express();
 const serverHttp = http.createServer(app);
-const io = new Server(serverHttp);
+const io = new Server(serverHttp,     
+    cors: {
+        origin: "https://jonatasfernandessilva7.github.io", // Permita requisições apenas do seu frontend
+        methods: ["GET", "POST"] // Métodos HTTP permitidos
+    }});
+
+const corsOptions = {
+    origin: 'https://jonatasfernandessilva7.github.io',
+    optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, '..', '/')));
 
